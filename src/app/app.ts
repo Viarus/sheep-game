cec354099bd121cb1af98adadeb779e83544287e
Viarus <videomontagefactory@gmail.com>
2025-11-27 19:29:26 +0100

    working on the form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { AppState } from './state/app/app.state';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Gender } from './models/sheep.model';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  private store = inject(Store);
-  protected fields$ = this.store.select(AppState.getSlices.fields);
+  private readonly store = inject(Store);
   protected readonly Gender = Gender;
+  protected readonly fields$ = this.store.select(AppState.getSlices.fields);
+  protected readonly newSheepForm = new FormGroup({
+    name: new FormControl<string>('', [Validators.required, Validators.maxLength(50)]),
+    gender: new FormControl<Gender>(Gender.Male, [Validators.required]),
+    isBranded: new FormControl<boolean>(false),
+    field: new FormControl<string | null>(null, [Validators.required]),
+  });
+
+  onAddSheep(): void {
+    console.log('Adding new Sheep');
+  }
 }
